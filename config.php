26243f0ac51d67c9c4fb9a2bd4bb3dfbbe2fa3c2
tasuku <tasuku1215mhg@gmail.com>
2018-09-28 15:35:41 +0900

    ADD

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST','localhost');
-define('DB_NAME','ph22');
-define('DB_USER','root');
-define('DB_PASS','root');
-define('UPLOAD_PATH','./images/')
-?>
--- js/main.js
+++ js/main.js
@@ -24,3 +24,47 @@
     }
   });
   })();
+  // (function (handleload) {
+  //   var xhr = new XMLHttpRequest;
+  
+  //   xhr.addEventListener('load',handleload, false);
+  //   xhr.open('GET', 'data.json', true);
+  //   xhr.send(null);
+  // }(function handleLoad (event) {
+  //   var xhr = event.target,
+  //       obj = JSON.parse(xhr.responseText);
+  
+  //   console.log(obj);
+  // }));
+// $.ajax({
+//   type: "get",
+//   url: "test.json",
+//   data: {name: 'chara'},
+//   dataType: "json",
+//   success: function(data, dataType){
+//     console.log($);
+//   }
+// });
+// $.ajax({
+//   url: '../data/Json.json',
+//   dataType : 'json'
+// });
+ $(function(){
+    $.ajax({ // json読み込み開始
+        type: 'GET',
+        url: '../data/Json.json',
+        dataType: 'json'
+        })
+        .then(
+        function(json) { // jsonの読み込みに成功した時
+            console.log('成功');
+            for(var i in json){
+                $("#output").append("<li>" + json[i].text +"</li>");
+            }
+        },
+        function() { //jsonの読み込みに失敗した時
+            console.log('失敗');
+        }
+    );
+});
+// console.log(data);
